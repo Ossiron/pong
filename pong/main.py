b4dc5c54294317d4410c961dbe43f1e6f5079de6
Ossiron <117552497+Ossiron@users.noreply.github.com>
2024-08-24 15:36:32 -0300

    Add files via upload

--- pong/main.py
+++ pong/main.py
@@ -8,13 +8,27 @@ window  = pygame.display.set_mode([1280,720])
 title = pygame.display.set_caption("pong")
 
 field = pygame.image.load("pong/assets/field.png")
-window.blit(field,(0,0))
+
 player1 = pygame.image.load("pong/assets/player1.png")
-window.blit(player1,(50,300))
+
 player2 = pygame.image.load("pong/assets/player2.png")
-window.blit(player2,(1150,300))
+
 ball = pygame.image.load("pong/assets/ball.png")
-window.blit(ball,(617,337))
+ball_x = 617
+ball_y = 337
+def draw():
+    window.blit(field,(0,0))
+    window.blit(player1,(50,300))
+    window.blit(player2,(1150,300))
+    window.blit(ball,(ball_x,ball_y))
+
+
+def move_ball():
+    global ball_x
+    global ball_y
+
+    ball_x+=1
+
 loop = True
 
 while loop:
@@ -22,5 +36,6 @@ while loop:
     for events in pygame.event.get():
         if events.type == pygame.QUIT:
             loop = False
-
+    draw()
+    move_ball()
     pygame.display.update()
